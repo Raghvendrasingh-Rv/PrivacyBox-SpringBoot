679170654fa77fa6bd69516fd2988b29d51d496b
Raghvendra Sengar <raghvendra.sengar@oracle.com>
2025-05-18 17:40:05 +0530

    tests deleted

--- src/test/java/net/rvOrg/privacyBox/PrivacyBoxApplicationTests.java
+++ src/test/java/net/rvOrg/privacyBox/PrivacyBoxApplicationTests.java
@@ -3,11 +3,7 @@ package net.rvOrg.privacyBox;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PrivacyBoxApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+class PrivacyBoxApplicationTests {
 
 }
